faea8beb392f7be7999347066c49616b5af15a6f
mike12699 <mariomania85@gmail.com>
2021-11-19 22:56:21 -0600

    Implements the code to the best of my ability (for some reason 'app' executable would not be created)

--- logic.cpp
+++ logic.cpp
@@ -2,31 +2,67 @@
 #include <cstdlib>
 #include <iostream>
 
-bool **create_board(const int height, const int width) {
+bool **create_board(const int height, const int width) 
+{
   bool **board = (bool **)malloc(sizeof(bool *) * height);
-  for (int i = 0; i < height; i++) {
+  for (int i = 0; i < height; i++) 
+  {
     board[i] = (bool *)malloc(sizeof(bool) * width);
   }
   return board;
 }
 
-void destroy_board(bool **board, const int height) {
-  for (int i = 0; i < height; ++i) {
+void destroy_board(bool **board, const int height) 
+{
+  for (int i = 0; i < height; i++) 
+  {
     free(board[i]);
   }
   free(board);
 }
 
-void place_ships(bool **board, const int shipcount, const int width,
-                 const int height) {
-  // TODO implement this function
+//Randomly places the ships on the 6 x 6 board
+void place_ships(bool **board, const int shipcount, const int width, const int height) 
+{
+  int ships = 0;
+  while(ships < shipcount)
+  {
+    int x = rand() % width;
+    int y = rand() % height;
+    if(board[x][y] != 1)
+    {
+      ships++;
+      board[x][y] = 1;
+    }
+  }
 }
 
-void find_ships(bool **board, const int width, const int height) {
-  // TODO implement
+void *find_ships(bool **board, const int width, const int height)
+{
+  for(int i = 0; i < height; i++)
+  {
+    guess(board, width, i);
+  }
+  return find_ships(board, width, height);
 }
 
-ResponseType guess(bool **board, const int x, const int y) {
-  // TODO implement this
-  return ResponseType::HIT;
+//Naturally this would display if the ship was either got a hit, near miss, or miss
+ResponseType guess(bool **board, const int x, const int y)
+{
+  find_ships(board, x, y);
+  if(board[x][y] == 0)
+  {
+    //std::cout << ResponseType::HIT << std::endl;
+    return ResponseType::HIT;
+  }
+  else if(board[x][y] == 0)
+  {
+    //std::cout << ResponseType::MISS << std::endl;
+    return ResponseType::MISS;
+  }
+  else
+  {
+    //std::cout << ResponseType::NEAR_MISS << std::endl;
+    return ResponseType::NEAR_MISS;
+  }
 }
--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
 #include "logic.h"
-int main() {
+#include <cstdlib>
+#include <iostream>
+int main() 
+{
+  int ships = 3;
   bool **board = create_board(6, 6);
   destroy_board(board, 6);
+  place_ships(board, ships, 6, 6);
+  guess(board, 6, 6);
 }
